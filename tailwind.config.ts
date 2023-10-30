eae31583f3a406fb94911b69ee6df8e3cfccdf1c
imstarbucks <spenwong98@gmail.com>
2023-10-30 23:23:52 +0800

    Update: Styling setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        background: 'var(--background)',
+        red: 'var(--red)',
+        green: 'var(--green)',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
