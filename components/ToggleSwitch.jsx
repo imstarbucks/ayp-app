b56935709efcb68c1039ae9851cb47053d17ee8c
imstarbucks <spenwong98@gmail.com>
2023-11-01 23:19:38 +0800

    Update: Toggle Button

--- components/ToggleSwitch.jsx
+++ components/ToggleSwitch.jsx
@@ -1,3 +1,18 @@
-const ToggleSwitch = ({ label }) => {
-  return <div>{label}</div>;
+const ToggleSwitch = ({ isActive }) => {
+  return (
+    <label htmlFor="employeeStatus">
+      <input
+        type="checkbox"
+        name="employeeStatus"
+        id="employeeStatus"
+        defaultChecked={isActive}
+        className="peer hidden"
+      />
+      <div className="w-8 h-4 rounded-lg relative peer-checked:bg-blue-500 bg-slate-600 flex peer-checked:justify-end justify-start">
+        <div className="w-4 h-4 rounded-full bg-white"></div>
+      </div>
+    </label>
+  );
 };
+
+export default ToggleSwitch;
