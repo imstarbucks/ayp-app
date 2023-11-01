c67f61c122dfb4d397d9e56c1bfd291b37cd2454
imstarbucks <spenwong98@gmail.com>
2023-11-01 23:19:24 +0800

    Update: Modal Context

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,97 +1,134 @@
-import { DialogHTMLAttributes, forwardRef, useRef, useState } from 'react';
+'use client';
+
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useContext,
+  createContext,
+} from 'react';
+
+import ToggleSwitch from '@/components/ToggleSwitch';
 
 import { Employees } from '@/types/Employees';
 
-type ModalProps = {
-  employeeId?: string;
-  employeeName?: string;
-  employeeEmail?: string;
-  employeeStatus?: boolean;
+const ModalContext = createContext();
+
+type ModalProps = Employees;
+
+const Modal: React.FC<ModalProps> = ({
+  name,
+  id,
+  email,
+  isActive,
+}: ModalProps) => {
+  const { modal, setModal } = useModal();
+
+  const closeBtnRef = useRef(null);
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  const handleCloseBtn = () => {
+    setModal(false);
+    modalRef?.current?.close();
+  };
+
+  useEffect(() => {
+    if (modal) {
+      modalRef.current?.showModal();
+    }
+
+    return;
+  }, [modal]);
+
+  return (
+    <dialog
+      ref={modalRef}
+      className="p-5 rounded-lg bg-secondary backdrop:bg-slate-900/30"
+    >
+      <div className="">
+        <div className="text-3xl">ID: {id}</div>
+        <form action="">
+          <div className="flex items-center my-5 gap-5 text-xl">
+            <label htmlFor="employeeName">Name: </label>
+            <input
+              id="employeeName"
+              name="employeeName"
+              className="px-3 py-1"
+              type="text"
+              defaultValue={name}
+              // onChange={(e) => employeeNameOnChange(e)}
+            />
+          </div>
+          <div className="flex items-center my-5 gap-5 text-xl">
+            <label htmlFor="employeeEmail">Email: </label>{' '}
+            <input
+              id="employeeEmail"
+              name="employeeEmail"
+              className="px-3 py-1"
+              type="email"
+              defaultValue={email}
+              // onChange={(e) => employeeEmailOnChange(e)}
+            />
+          </div>
+          <div className="flex items-center my-5 gap-5 text-xl">
+            <label>Status: </label>
+            <input type="checkbox" defaultChecked={isActive ? true : false} />
+            <ToggleSwitch isActive={isActive} />
+          </div>
+          <button
+            ref={closeBtnRef}
+            onClick={() => handleCloseBtn()}
+            className="bg-red px-3 py-2 rounded-md text-secondary me-5"
+            type="button"
+          >
+            Close
+          </button>
+          <button
+            className="bg-green px-3 py-2 rounded-md text-secondary"
+            type="submit"
+          >
+            Update
+          </button>
+        </form>
+      </div>
+    </dialog>
+  );
 };
 
-const Modal = forwardRef(
-  (
-    { employeeName, employeeEmail, employeeStatus, employeeId }: ModalProps,
-    ref
-  ) => {
-    const closeBtnRef = useRef(null);
-    const [employeeInputName, setEmployeeInputName] = useState<string>(
-      employeeName ? employeeName : ''
-    );
-    const [employeeInputEmail, setEmployeeEmail] = useState<string>(
-      employeeEmail ? employeeEmail : ''
-    );
+type ProviderProps = {
+  children: React.ReactNode;
+};
 
-    const employeeNameOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setEmployeeInputName(e.currentTarget.value);
-    };
+const ModalProvider: React.FC<ProviderProps> = (props) => {
+  const [modalData, setModalData] = useState<Employees>({
+    name: '',
+    email: '',
+    id: '',
+    isActive: false,
+  });
+  const [modal, setModal] = useState<boolean>(false);
 
-    const employeeEmailOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setEmployeeEmail(e.currentTarget.value);
-    };
+  return (
+    <ModalContext.Provider value={{ setModalData, modal, setModal }} {...props}>
+      <Modal
+        name={modalData.name}
+        email={modalData.email}
+        id={modalData.id}
+        isActive={modalData.isActive}
+      />
+      {props.children}
+    </ModalContext.Provider>
+  );
+};
 
-    const handleCloseBtn = () => {
-      ref?.current.close();
-    };
-
-    return (
-      <dialog
-        ref={ref}
-        className="w-1/2 p-5 rounded-lg bg-secondary backdrop:bg-slate-900/30"
-      >
-        <div className="">
-          <div className="text-3xl">ID: {employeeId}</div>
-          <form action="">
-            <div className="flex items-center my-5 gap-5 text-xl">
-              <label htmlFor="employeeName">Name: </label>
-              <input
-                id="employeeName"
-                name="employeeName"
-                className="px-3 py-1"
-                type="text"
-                value={employeeInputName}
-                defaultValue={employeeInputName}
-                onChange={(e) => employeeNameOnChange(e)}
-              />
-            </div>
-            <div className="flex items-center my-5 gap-5 text-xl">
-              <label htmlFor="employeeEmail">Email: </label>{' '}
-              <input
-                id="employeeEmail"
-                name="employeeEmail"
-                className="px-3 py-1"
-                type="email"
-                value={employeeInputEmail}
-                defaultValue={employeeInputEmail}
-                onChange={(e) => employeeEmailOnChange(e)}
-              />
-            </div>
-            <div className="flex items-center my-5 gap-5 text-xl">
-              <label>Status: </label>
-              <input
-                type="checkbox"
-                checked={employeeStatus ? true : false}
-                defaultChecked={false}
-              />
-            </div>
-            <button
-              ref={closeBtnRef}
-              onClick={() => handleCloseBtn()}
-              className="bg-red px-3 py-2 rounded-sm text-secondary me-5"
-              type="button"
-            >
-              Close
-            </button>
-            <button
-              className="bg-green px-3 py-2 rounded-sm text-secondary"
-              type="submit"
-            >
-              Update
-            </button>
-          </form>
-        </div>
-      </dialog>
-    );
+const useModal = () => {
+  const context = useContext(ModalContext);
+  if (context === undefined) {
+    throw new Error('useModal must be used within a UserProvider');
   }
-);
+
+  return context;
+};
+
 export default Modal;
+export { ModalProvider, useModal };
